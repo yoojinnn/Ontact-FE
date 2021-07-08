951e37174a6707e6c86dda4f05499ada8a55cc7f
yoojinnn <98ashley@naver.com>
2021-07-08 14:17:23 +0900

    api index.js 코드 변경 - API_ADDRESS를 REACT_APP_API_ADDRESS로 변경

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.API_ADDRESS,
+  baseURL: process.env.REACT_APP_API_ADDRESS,
 });
 
 async function searchUser(userId) {
